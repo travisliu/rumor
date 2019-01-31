50671c558b59548b2042744f313ba3c52a29ee7a
travisliu <travisliu.tw@gmail.com>
2019-01-31 21:20:57 +0000

    Extract Server class

--- index.js
+++ index.js
@@ -1,42 +1,15 @@
-const http = require('http')
+const Server = require('./lib/server')
 
-const react = (event) => {
-  if (event.type == "message") {
+const path   = process.env.WEBHOOK_PATH || '/linewebhook'
+const port   = process.env.WEBHOOK_PORT ||  3030
+const server = new Server()
 
-  }
-}
-
-const server = http.createServer((req, res) => {
-
-  const path = process.env.WEBHOOK_PATH || '/linewebhook'
-
-  const signature = req.headers['x-line-signature']
-  res.setHeader('X-Powered-By', 'linebot')
-  if (req.method === 'POST' && req.url === path) {
-    var body = ''
-    req.on('data', data => {
-      body += data
-
-      if (body.length > 1e6) req.connection.destroy()
-    })
-
-    req.on('end', () => {
-      const payload = JSON.parse(body)
-      for(let i = 0; i < payload.events.length; i++) {
-        react(payload.events[i])
-      }
-    })
-
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    return res.end('{}')
-  } else {
-    res.statusCode = 404
-    res.setHeader('Content-Type', 'text/html; charset=utf-8')
-    return res.end('Not found')
-  }
+server.on('message', event => {
+  console.log(`message: ${JSON.stringify(event)}`)
 })
 
-const port = process.env.WEBHOOK_PORT || 1987
+server.on('intro', event => {
+  console.log(`intro: ${JSON.stringify(event)}`)
+})
 
-server.listen(port)
+server.listen(path, port)
